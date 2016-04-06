2a368b83c2db591855e2a614f148914617f8de6c
kalyan chakravarthy <kalyan711987@gmail.com>
2016-04-06 21:41:24 +0530

    1 . Pull to refresh for menu 2. added to order page - if dish length is too big - it just shows dots - very confusing 3. SEARCH BAR  should be visible only if user pulls down the screen

--- EnjoyFresh/DishesViewController.m
+++ EnjoyFresh/DishesViewController.m
@@ -37,8 +37,8 @@ CGPoint location;
 {
 }
 @property(nonatomic, strong, readwrite) PayPalConfiguration *payPalConfig;
-
-
+@property(nonatomic)UIRefreshControl *refreshControl;
+@property(nonatomic)CGPoint lastContentOffset;
 @end
 static UIColor *favcolor;
 static UIColor *unfavcolor;
@@ -126,7 +126,14 @@ static UIColor *unfavcolor;
     {
         [dishes_Tbl setFrame:CGRectMake(0, 64, 320, 510)];
     }
-    
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    self.refreshControl.backgroundColor = [UIColor lightGrayColor];
+    self.refreshControl.tintColor = [UIColor colorWithRed:138/255.0 green:188/255.0 blue:105/255.0 alpha:1];
+    [self.refreshControl addTarget:self
+                            action:@selector(getDishes)
+                  forControlEvents:UIControlEventValueChanged];
+    [self.scrollView addSubview:self.refreshControl];
+
     
     categoryArr=[[NSMutableArray alloc]init];
     dietryStr=@"";
@@ -154,7 +161,7 @@ static UIColor *unfavcolor;
     
     
     search_bar=[[UISearchBar alloc]initWithFrame:CGRectMake(0,0, 320, 40)];
-    [self.scrollView addSubview:search_bar];
+    [self.view addSubview:search_bar];
 
     search_bar.delegate=self;
     search_bar.searchBarStyle=UISearchBarStyleMinimal;
@@ -724,6 +731,7 @@ static UIColor *unfavcolor;
             break;
         case 2:
         {
+            [self.refreshControl endRefreshing];
             if (errMsg)
             {
                 NSLog(@"Dish Listing Result: %@", result);
@@ -1066,6 +1074,7 @@ static UIColor *unfavcolor;
 }
 -(void)dataDidFailedLoadingWithError:(NSString *)err
 {
+    [self.refreshControl endRefreshing];
     [hud hide:YES];
     [hud removeFromSuperview];
     hud=nil;
@@ -1423,7 +1432,24 @@ static UIColor *unfavcolor;
     CGRect rect = searchBar.frame;
     rect.origin.y = MIN(0, self.scrollView.contentOffset.y);
     searchBar.frame = rect;
+    
+    CGPoint currentOffset = scrollView.contentOffset;
+    if (currentOffset.y > self.lastContentOffset.y && currentOffset.y>10)
+    {
+        // Upward
+        [self hideSearchBar];
+
+    }
+    else
+    {
+        [self showSearchBar];
+
+        // Downward
+    }
+    self.lastContentOffset = currentOffset;
+
 }
+
 #pragma mark
 #pragma mark - Button Actions
 - (void)shareClicked:(id)sender
@@ -3141,7 +3167,21 @@ BOOL clearClick=NO;
     [hud removeFromSuperview];
     hud = nil;
 }
+-(void)hideSearchBar{
+    
+    
+         search_bar.frame
+         = CGRectMake(0, -100,
+                      search_bar.frame.size.width, search_bar.frame.size.height);
+}
+-(void)showSearchBar{
 
+    search_bar.frame
+         = CGRectMake(0, 65,
+                      search_bar.frame.size.width, search_bar.frame.size.height);
+    
+
+}
 #pragma mark
 #pragma mark - TextField Delegates
 
